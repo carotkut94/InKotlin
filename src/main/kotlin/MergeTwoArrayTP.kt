29cf7475f6e3e2041bdf52680d393becc73d6fe3
Sidhant Rajora <sidhantrajora@Sidhants-MacBook-Pro.local>
2022-06-24 21:44:28 +0530

    another day another simple problem

---
+++ src/main/kotlin/MergeTwoArrayTP.kt
@@ -0,0 +1,29 @@
+fun main(){
+
+    val array1 = intArrayOf(1,5,7,9)
+    val array2 = intArrayOf(2,3,4,8,10)
+    val mergedArray = mutableListOf<Int>()
+    var p1 = 0
+    var p2 = 0
+
+    val maxLen = array2.size
+
+    while(p1 < maxLen){
+        if(p2 < array1.size){
+            if(array2[p1]<array1[p2]){
+                mergedArray.add(array2[p1])
+                p1+=1
+            }else{
+                mergedArray.add(array1[p2])
+                p2+=1
+            }
+        }else{
+            mergedArray.add(array2[p1])
+            p1+=1
+        }
+    }
+
+    mergedArray.forEach{
+        println(it)
+    }
+}
