cafac6de00253ccc62ac51c9264ed3ad39562a1e
carotkut94 <carotkut12@gmail.com>
2022-06-28 22:41:56 +0530

    feat(75d3-p1): inplace karte hai merge

--- src/main/kotlin/MergeLinkedLists.kt
+++ src/main/kotlin/MergeLinkedLists.kt
@@ -1,4 +1,4 @@
-class ListNode(var value: Int) {
+class ListNode(var `val`: Int) {
     var next: ListNode? = null
 }
 
@@ -8,7 +8,7 @@ class MergeLinkedLists {
     private fun merge(list1: ListNode?, list2: ListNode?) : ListNode?{
         if(list1 == null) return list2
         if(list2 == null) return list1
-        return if(list1.value<=list2.value){
+        return if(list1.`val`<=list2.`val`){
             list1.next = merge(list1.next, list2)
             list1
         }else{
@@ -18,6 +18,28 @@ class MergeLinkedLists {
     }
 
     fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
-        return merge(list1, list2)
+        var l1 = list1
+        var l2 = list2
+        if(l1==null) return l2
+        if(l2==null) return l1
+
+        if(l1.`val` > l2.`val`){
+            val temp = l1
+            l1 = l2
+            l2 = temp
+        }
+        val result = l1
+        while (l1!=null && l2!=null){
+            var tmp:ListNode? = null
+            while (l1!=null && l1.`val` <= l2.`val`){
+                tmp = l1
+                l1 = l1.next
+            }
+            tmp?.next = l2
+            val temp = l1
+            l1 = l2
+            l2 = temp
+        }
+        return result
     }
 }
