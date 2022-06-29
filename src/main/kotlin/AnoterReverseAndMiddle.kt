5a097dead8783afc23cbb7020dd5980a2a1196ff
carotkut94 <carotkut12@gmail.com>
2022-06-29 23:28:51 +0530

    feat(75d4-p1): slow fast middle linked list?

--- src/main/kotlin/AnoterReverseAndMiddle.kt
+++ src/main/kotlin/AnoterReverseAndMiddle.kt
@@ -1,14 +1,49 @@
 class AnotherReverseAndMiddle {
-        fun reverseList(head: ListNode?): ListNode? {
-            var current = head
-            var previous:ListNode? = null
-            while(current!=null){
-                val temp = current.next
-                current.next = previous
-                previous = current
-                current = temp
-            }
-
-            return previous
+    fun reverseList(head: ListNode?): ListNode? {
+        var current = head
+        var previous: ListNode? = null
+        while (current != null) {
+            val temp = current.next
+            current.next = previous
+            previous = current
+            current = temp
         }
+
+        return previous
+    }
+
+    fun middleNode(head: ListNode?): ListNode? {
+        var l = 0
+        var current = head
+        while (current != null) {
+            l += 1
+            current = current.next
+        }
+        if (l == 1) {
+            return head
+        }
+
+        val middle = l / 2
+        current = head
+        l = 0
+        while (current != null) {
+            l += 1
+            if (l == middle) {
+                return current.next
+            } else {
+                current = current.next
+            }
+        }
+        return current
+    }
+
+    fun middleSlowFast(head: ListNode?):ListNode?{
+        var slow = head
+        var fast = head
+        while(fast?.next != null){
+            slow = slow?.next
+            fast = fast.next?.next
+        }
+        return slow
+    }
 }
