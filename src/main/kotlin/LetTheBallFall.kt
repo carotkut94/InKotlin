9d63a37a22fc035bf9e038a7f336742c65470954
carotkut94 <carotkut12@gmail.com>
2022-07-04 23:38:42 +0530

    feat(75d7-p1): lets do some dfs

---
+++ src/main/kotlin/LetTheBallFall.kt
@@ -0,0 +1,29 @@
+class LetTheBallFall {
+
+    fun findBall(grid: Array<IntArray>): IntArray {
+        val resultArray = IntArray(grid[0].size)
+
+        for(column in grid[0].indices){
+           resultArray[column] = dfs(grid, 0, column)
+        }
+
+        return resultArray
+    }
+
+    private fun dfs(grid: Array<IntArray>, row: Int, column: Int): Int {
+        if(row == grid.size){
+            return column
+        }
+
+        if(column<0 || column>=grid[0].size){
+            return  -1
+        }
+
+        if(grid[row][column] == 1 && column<grid[0].size-1 && grid[row][column+1]==grid[row][column]){
+            return dfs(grid, row+1, column+1)
+        }else if(grid[row][column] == -1 && column>0 && grid[row][column-1]==grid[row][column]){
+            return dfs(grid, row+1, column-1)
+        }
+        return  -1
+    }
+}
